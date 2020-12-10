12b90d94642dd7e21985b711d572d9525cc4571a
Roman Penyaev <r.peniaev@gmail.com>
2020-12-10 21:55:41 +0100

    test-psu: read and show voltage

--- test-psu.c
+++ test-psu.c
@@ -29,34 +29,53 @@ int main(int argc, char *argv[])
 	float voltage, current;
 	int rc;
 
-	if (argc != 3) {
+	if (argc != 1 && argc != 3) {
 		usage();
 		return 1;
 	}
-	rc = sscanf(argv[1], "%f", &voltage);
-	if (rc != 1) {
-		usage();
-		return 1;
+	if (argc == 3) {
+		rc = sscanf(argv[1], "%f", &voltage);
+		if (rc != 1) {
+			usage();
+			return 1;
+		}
+		rc = sscanf(argv[2], "%f", &current);
+		if (rc != 1) {
+			usage();
+			return 1;
+		}
 	}
-	rc = sscanf(argv[2], "%f", &current);
-	if (rc != 1) {
-		usage();
-		return 1;
-	}
+
 	rc = sky_psu_init(&conf, &psu);
 	if (rc) {
 		printf("sky_psu_init: failed!\n");
 		return 1;
 	}
-	rc = sky_psu_set_voltage(&psu, voltage);
-	if (rc) {
-		printf("sky_psu_set_voltage: failed!\n");
-		return 1;
-	}
-	rc = sky_psu_set_current(&psu, current);
-	if (rc) {
-		printf("sky_psu_set_current: failed!\n");
-		return 1;
+
+	if (argc == 3) {
+		rc = sky_psu_set_voltage(&psu, voltage);
+		if (rc) {
+			printf("sky_psu_set_voltage: failed!\n");
+			return 1;
+		}
+		rc = sky_psu_set_current(&psu, current);
+		if (rc) {
+			printf("sky_psu_set_current: failed!\n");
+			return 1;
+		}
+	} else {
+		rc = sky_psu_get_voltage(&psu, &voltage);
+		if (rc) {
+			printf("sky_psu_get_voltage: failed!\n");
+			return 1;
+		}
+		rc = sky_psu_get_current(&psu, &current);
+		if (rc) {
+			printf("sky_psu_get_current: failed!\n");
+			return 1;
+		}
+
+		printf("Voltage %.1fV\nCurrent %.1fA\n", voltage, current);
 	}
 	sky_psu_deinit(&psu);
 
