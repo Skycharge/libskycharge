5e1b83fc701640667697272f5201a31d672decf1
Roman Penyaev <r.peniaev@gmail.com>
2021-02-01 19:29:22 +0100

    daemon: move common daemon related functions to header

--- skybroker.c
+++ skybroker.c
@@ -8,6 +8,7 @@
 #include <czmq.h>
 
 #include "skybroker-cmd.h"
+#include "daemon.h"
 #include "version.h"
 #include "skyproto.h"
 
@@ -22,76 +23,6 @@ struct server_peer {
 		struct server_peer *next;
 	};
 };
-
-/* TODO: same function exists in skyserver.c */
-static int sky_pidfile_create(const char *pidfile, int pid)
-{
-	int rc, fd, n;
-	char buf[16];
-
-	fd = open(pidfile, O_CREAT | O_EXCL | O_TRUNC | O_WRONLY, 0600);
-	if (fd < 0) {
-		sky_err("sky_pidfile_create: failed open(%s) errno=%d\n",
-			pidfile, errno);
-
-		return -1;
-	}
-	n = snprintf(buf, sizeof(buf), "%u\n", pid);
-	rc = write(fd, buf, n);
-	if (rc < 0) {
-		sky_err("sky_pidfile_create: failed write(%s) errno=%d\n",
-			pidfile, errno);
-		close(fd);
-
-		return -1;
-	}
-	close(fd);
-
-	return 0;
-}
-
-/* TODO: same function exists in skyserver.c */
-/*
- * This routine is aimed to be used instead of standard call daemon(),
- * because we want to create pidfile from a parent process not to race
- * with a systemd PIDFile handler.
- */
-static int sky_daemonize(const char *pidfile)
-{
-	int pid, rc;
-
-	pid = fork();
-	if (pid == -1) {
-		sky_err("sky_daemonize: fork() failed, errno=%d\n", errno);
-		return -1;
-	}
-	else if (pid != 0) {
-		if (pidfile) {
-			/*
-			 * Yes, we write pid from a parent to avoid any races
-			 * inside systemd PIDFile handlers.  In case of errors
-			 * child is gracefully killed.
-			 */
-
-			rc = sky_pidfile_create(pidfile, pid);
-			if (rc) {
-				kill(pid, SIGTERM);
-
-				return -1;
-			}
-		}
-		/* Parent exits */
-		exit(0);
-	}
-	if (setsid() == -1) {
-		sky_err("sky_daemonize: setsid() failed, errno=%d\n", errno);
-		return -1;
-	}
-	pid = chdir("/");
-	(void)pid;
-
-	return 0;
-}
 
 /**
  * sky_find_data_frame() - finds first data frame, skips all
--- skyserver.c
+++ skyserver.c
@@ -18,6 +18,7 @@
 #include "skyserver-cmd.h"
 #include "libskysense.h"
 #include "libskypsu.h"
+#include "daemon.h"
 #include "avahi.h"
 #include "skyproto.h"
 #include "version.h"
@@ -50,74 +51,6 @@ struct sky_server {
 	struct sky_psu global_psu;
 	struct avahi *avahi;
 };
-
-static int sky_pidfile_create(const char *pidfile, int pid)
-{
-	int rc, fd, n;
-	char buf[16];
-
-	fd = open(pidfile, O_CREAT | O_EXCL | O_TRUNC | O_WRONLY, 0600);
-	if (fd < 0) {
-		sky_err("sky_pidfile_create: failed open(%s) errno=%d\n",
-			pidfile, errno);
-
-		return -1;
-	}
-	n = snprintf(buf, sizeof(buf), "%u\n", pid);
-	rc = write(fd, buf, n);
-	if (rc < 0) {
-		sky_err("sky_pidfile_create: failed write(%s) errno=%d\n",
-			pidfile, errno);
-		close(fd);
-
-		return -1;
-	}
-	close(fd);
-
-	return 0;
-}
-
-/*
- * This routine is aimed to be used instead of standard call daemon(),
- * because we want to create pidfile from a parent process not to race
- * with a systemd PIDFile handler.
- */
-static int sky_daemonize(const char *pidfile)
-{
-	int pid, rc;
-
-	pid = fork();
-	if (pid == -1) {
-		sky_err("sky_daemonize: fork() failed, errno=%d\n", errno);
-		return -1;
-	}
-	else if (pid != 0) {
-		if (pidfile) {
-			/*
-			 * Yes, we write pid from a parent to avoid any races
-			 * inside systemd PIDFile handlers.  In case of errors
-			 * child is gracefully killed.
-			 */
-
-			rc = sky_pidfile_create(pidfile, pid);
-			if (rc) {
-				kill(pid, SIGTERM);
-
-				return -1;
-			}
-		}
-		/* Parent exits */
-		exit(0);
-	}
-	if (setsid() == -1) {
-		sky_err("sky_daemonize: setsid() failed, errno=%d\n", errno);
-		return -1;
-	}
-	pid = chdir("/");
-	(void)pid;
-
-	return 0;
-}
 
 static int sky_kill_pthread(pthread_t thread)
 {
